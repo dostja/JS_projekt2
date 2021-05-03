62f1289d899df91ae36e51f65336a382c736fc81
dostja <dostalova2jana@seznam.cz>
2021-05-03 19:29:55 +0200

    finish

---
+++ kviz.js
@@ -0,0 +1,150 @@
+let moznost = document.getElementById("odpovedi");
+let indexOtazky;
+let pocetSpravnychOdpovedi;
+let poleOdpovedi = [];
+let seznamOtazek = [
+  {
+    textOtazky: "Jak se jmenuje její pes?",
+    obrazek: "obrazky/moncicak.jpg",
+    mozneOdpovedi: ["Ramba", "Mamba", "Namba"],
+    spravnaOdpoved: 1,
+  },
+  {
+    textOtazky: "Kolik je jí let na fotce?",
+    obrazek: "obrazky/pivo.jpg",
+    mozneOdpovedi: ["5", "8", "25"],
+    spravnaOdpoved: 1,
+  },
+  {
+    textOtazky: "Jaká je její nejoblíbenější barva?",
+    obrazek: "obrazky/ovoce.jpg",
+    mozneOdpovedi: ["cyklámenová", "hořčicová", "petrolejová"],
+    spravnaOdpoved: 2,
+  },
+  {
+    textOtazky: "Jaké je její povolání?",
+    obrazek: "obrazky/snehurka.jpg",
+    mozneOdpovedi: ["Učitelka", "Kadeřnice", "Oděvní návrhář"],
+    spravnaOdpoved: 2,
+  },
+  {
+    textOtazky: "Kolik v životě chovala dětí?",
+    obrazek: "obrazky/pivo.jpg",
+    mozneOdpovedi: ["10", "7", "3"],
+    spravnaOdpoved: 2,
+  },
+];
+window.onload = function nacteniStranky() {
+  indexOtazky = 0;
+  pocetSpravnychOdpovedi = 0;
+  novaOtazka(indexOtazky);
+};
+
+function novaOtazka(iOtazky) {
+  let otazkaObj = seznamOtazek[iOtazky];
+  let titulekOtazky = document.getElementById("otazka");
+  titulekOtazky.innerHTML = otazkaObj.textOtazky;
+  let cisloOtazky = document.getElementById("cislo");
+  cisloOtazky.innerHTML = iOtazky + 1;
+  let jednotliveOdpovedi = document.getElementById("odpovedi");
+  while (jednotliveOdpovedi.firstChild) {
+    jednotliveOdpovedi.removeChild(jednotliveOdpovedi.lastChild);
+  }
+
+  for (let i = 0; i < seznamOtazek[iOtazky].mozneOdpovedi.length; i++) {
+    let odpoved = document.createElement("li");
+    odpoved.setAttribute("id", "odpoved" + i);
+    odpoved.onclick = stiskMoznosti;
+    odpoved.innerHTML = seznamOtazek[iOtazky].mozneOdpovedi[i];
+    jednotliveOdpovedi.appendChild(odpoved);
+  }
+}
+
+function stiskMoznosti(e) {
+  let indexOdpovedi = e.srcElement.getAttribute("id").replace("odpoved", "");
+  let spravnaOdpoved = seznamOtazek[indexOtazky].spravnaOdpoved;
+  let vysledek = indexOdpovedi == spravnaOdpoved;
+
+  console.log(vysledek);
+  if (vysledek == true) {
+    pocetSpravnychOdpovedi++;
+  }
+
+  poleOdpovedi[indexOtazky] = indexOdpovedi;
+
+  console.log(pocetSpravnychOdpovedi);
+
+  if (indexOtazky == seznamOtazek.length - 1) {
+    ukazVysledek();
+  } else {
+    dalsiOtazka();
+  }
+}
+
+function dalsiOtazka() {
+  indexOtazky++;
+  novaOtazka(indexOtazky);
+  let fotka = document.getElementById('obrazek');
+  if (indexOtazky == 1){
+    
+    fotka.src = '/obrazky/DSC_0001.JPG';
+  }
+  else if (indexOtazky == 2)
+  {fotka.src = '/obrazky/saty.png';}
+  else if (indexOtazky == 3)
+  {fotka.src = '/obrazky/povolani.jpeg';}
+  else if (indexOtazky == 4)
+  {fotka.src = '/obrazky/dite.jpg';}
+}
+
+function ukazVysledek() {
+  //document.getElementById("kviz").innerHTML = "";
+  document.getElementById("kviz").style.display = "none";
+  document.querySelector(".konecne-vysledky").style.display = "inline";
+  //let konecneOdpovedi = document.getElementById('vysledek');
+  document.getElementById("vysledek-h2");
+  let vysledkyDiv = document.getElementById("konecne-odpovedi");
+
+  for (let i = 0; i < seznamOtazek.length; i++) {
+    let otazksaObj = seznamOtazek[i];
+    let divOtazka = document.createElement("div");
+
+    let nadpisOtazky = document.createElement("li");
+    nadpisOtazky.classList.add('polozky');
+    nadpisOtazky.classList.add("otazka");
+    nadpisOtazky.innerHTML = otazksaObj.textOtazky;
+    divOtazka.appendChild(nadpisOtazky);
+
+    let tvojeOdpoved = document.createElement("li");
+    tvojeOdpoved.classList.add('polozky');
+    let tvojeVolba = seznamOtazek[i].mozneOdpovedi[poleOdpovedi[i]];
+    tvojeOdpoved.innerHTML = "Tvoje odpověď: " + tvojeVolba;
+    divOtazka.appendChild(tvojeOdpoved);
+
+    let spravnostOdpovedi = document.createElement("li");
+    spravnostOdpovedi.classList.add('polozky');
+    let spravnost = seznamOtazek[i].spravnaOdpoved == poleOdpovedi[i];
+    if (spravnost) {
+      spravnostOdpovedi.innerHTML = "Tato odpověď je správně";
+    } else {
+      spravnostOdpovedi.innerHTML =
+        "Správná odpověď: " +
+        seznamOtazek[i].mozneOdpovedi[seznamOtazek[i].spravnaOdpoved];
+    }
+    divOtazka.appendChild(spravnostOdpovedi);
+
+    vysledkyDiv.appendChild(divOtazka);
+  }
+
+  let pocetSpravnychOdpovediVypis = document.getElementById("vyhodnoceni");
+  let vypocetProcent = (pocetSpravnychOdpovedi / seznamOtazek.length) * 100;
+  pocetSpravnychOdpovediVypis.innerHTML =
+    "Správně je " +
+    pocetSpravnychOdpovedi +
+    " z " +
+    seznamOtazek.length +
+    " otázek. " +
+    "Úspěšnost " +
+    vypocetProcent +
+    " %.";
+}
